bae4bc1d8041e1804603b92e870834f871fdf2a7
Alexander Spanos <alexisspa9@gmail.com>
2018-03-20 15:14:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,3 +12,14 @@ WIN_COMBINATIONS = [[0,1,2],
                     [2,5,8],
                     [0,4,8],
                     [2,4,6]]
+                    
+                    
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    if (combination[0] == "X" && combination[1] == "X" && combination[2] == "X" || combination[0] == "Y" && combination[1] == "Y" && combination[2] == "Y")
+      return combination
+      else
+      return false
+    end
+  end
+end
